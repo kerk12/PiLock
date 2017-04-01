872207fd0a942716d59f5b8a2b28a0f7f0470857
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-04-01 14:02:46 +0300

    Added gitignore

--- main/models.py
+++ main/models.py
@@ -7,4 +7,3 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     authToken = models.CharField(max_length=64, blank=True, null=False)
-
