d7734a1373a58505b9e056d0f1bde989b40728ed
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-04-25 20:45:41 +0300

    Secured setup_wsgi.sh

--- setup_wsgi.sh
+++ setup_wsgi.sh
@@ -3,5 +3,13 @@
 cp apacheconf/pilock.conf /etc/apache2/sites-available/pilock.conf
 
 sudo -u www-data python manage.py migrate
+chown www-data:www-data db.sqlite3
+chmod 700 db.sqlite3
+
 a2ensite pilock
 service apache2 restart
+
+usermod -a -G dialout www-data
+
+echo 'Restarting the system'
+reboot
