df9da274018c173d87ec7005e4a2b3f7ed42f6c2
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-07-31 00:12:33 +0300

    Changed forgotten debug mode to false.

--- PiLock/settings.py
+++ PiLock/settings.py
@@ -43,20 +43,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = getSecretKey()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # HSTS: Change this to the max age defined by the server.
 # Comment it to disable HSTS. (NOT recommended)
-# SECURE_HSTS_SECONDS = 63072000
+SECURE_HSTS_SECONDS = 63072000
 
 # SSL Redirecting
 # CAUTION: Needs to be set to True when using SSL. This will redirect all the traffic to HTTPS.
 # Set to False when you are not using SSL.
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 
 # Secure session cookie
 # Encrypts session cookies. Recommended: True
-# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 
 ALLOWED_HOSTS = ["*"]
