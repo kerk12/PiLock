964aa3222e01ea3fca8f632f46525cea325b04da
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-07-31 00:22:50 +0300

    Added STATIC_ROOT

--- PiLock/settings.py
+++ PiLock/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = '/var/www/PiLock/static/'
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/AdminCP/login'
