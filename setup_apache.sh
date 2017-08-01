bb133fed843071109c99bd4422daca7a8009a31d
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-08-01 23:34:51 +0300

    Fixed both setup files: Project files are now copied to apache root.

--- setup_apache.sh
+++ setup_apache.sh
@@ -7,12 +7,22 @@ then
 fi
 
 echo 'Installing required packages'
-
+sleep 2s
 apt-get update
 apt-get -y install apache2 libapache2-mod-wsgi python-pip
 a2enmod wsgi
+
+echo 'Creating the project directory in /var/www and copying files...'
+sleep 2s
+mkdir -p /var/www/PiLock
+cp -r ./* /var/www/PiLock
+
+echo 'Installing Python Package requirements...'
+sleep 2s
 pip install -r requirements.txt
 
+echo 'Please answer these questions, so that we can create an SSL certificate. Remember that the FQDN (or common name) will be the final address that the server will be available at.'
+sleep 3s
 mkdir /etc/apache2/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/pilock.key -out /etc/apache2/ssl/pilock.crt
 chmod -R 400 /etc/apache2/ssl
--- setup_wsgi.sh
+++ setup_wsgi.sh
@@ -6,20 +6,31 @@ then
 	exit 1
 fi
 
+echo 'Copying the main PiLock configuration file for Apache...'
+sleep 1s
 cp apacheconf/pilock.conf /etc/apache2/sites-available/pilock.conf
+
+echo 'Performing DB Migration...'
+sudo -u www-data python manage.py migrate
+
+echo 'Answer yes to the following, this is needed in order to collect all the image,js and CSS files for the server to function properly.'
+sleep 5s
+sudo -u www-data python manage.py collectstatic
 
 echo 'You will now be asked to create a superuser for your server. This is the default user used when managing the server with the admin interface.'
 sleep 5s
-
-sudo -u www-data python manage.py migrate
 sudo -u www-data python manage.py createsuperuser
 chown www-data:www-data db.sqlite3
 chmod 700 db.sqlite3
 
+echo 'Enabling the main PiLock Apache configuration file...'
 a2ensite pilock
+echo 'Restarting Apache'
 service apache2 restart
 
+echo 'Adding the www-data user to the dialout group (needed for the Serial Port to function)...'
 usermod -a -G dialout www-data
 
-echo 'Restarting the system'
+echo 'Restarting the system in 5 seconds...'
+sleep 5s
 reboot
