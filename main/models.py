0fb7e75f71e3f96a5c334abd001eb56551000758
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-09-07 02:24:15 +0300

    Made authToken never null or blank.

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    authToken = models.CharField(max_length=64, blank=True, null=False)
+    authToken = models.CharField(max_length=64)
     pin = models.CharField(max_length=6, default="123456")
 
 
