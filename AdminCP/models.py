9d92460357faf946e9ff7369bce515da3bc0dc3b
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-10-06 12:53:26 +0300

    Added the notification model.

--- AdminCP/models.py
+++ AdminCP/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+class Notification(models.Model):
+    TYPES = (
+        ("DEBUG", "Debug Mode"),
+        ("UPDATE", "Update available"),
+        ("SEC", "Security Issue"),
+    )
+
+    # Only one notification created for each type.
+    type = models.CharField(max_length=10, choices=TYPES, default="DEBUG", unique=True)
+    created = models.DateTimeField(auto_now_add=True)
